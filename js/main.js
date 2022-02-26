593c2a18bc11a90bde8e6bc9ae000cc3c4f89ac5
Omar El Sayed <90359498+themattricks@users.noreply.github.com>
2022-02-26 17:27:48 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,7 +43,8 @@ theAddButton.onclick = function() {
 document.querySelector('.new-task').onkeypress = function(e) {
     if (!e) e = window.event;
     if (e.keyCode == '13') {
-        globalThis.tasksArray.push(this.value);
+        let tasksArray = localStorage.getItem("tasks").split(',');
+        tasksArray.push(this.value);
         localStorage.setItem("tasks", tasksArray);
         document.querySelector('.tasks').innerHTML = "";
         refreshTasks();
